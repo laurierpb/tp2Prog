4cf9bc0f5b2cf8fd7e1e3b5657ca569435412519
Laurier Paquette-Berube <laurier.p.b@gmail.com>
2014-03-25 11:44:48 -0400

    Merge origin/master
    Conflicts:
    	build.xml
    	nbproject/build-impl.xml
    	nbproject/genfiles.properties
    	nbproject/project.properties
    	nbproject/project.xml

--- src/ListeDoublonsChainee.java
+++ src/ListeDoublonsChainee.java
@@ -6,7 +6,6 @@ import java.util.ArrayList;
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 /**
  *
  * @author Laurier
@@ -18,6 +17,9 @@ public class ListeDoublonsChainee implements ListeDoublons<Object> {
 
     //pointe sur le premier des doublons en fin de liste 
     int nbrElements = 0; //nombre d’éléments distincts dans la liste 
+
+    public ListeDoublonsChainee() {
+    }
 
     @Override
     public boolean estVide() {
@@ -31,32 +33,39 @@ public class ListeDoublonsChainee implements ListeDoublons<Object> {
 
     @Override
     public void ajouter(Object element) {
+
         MaillonD temp = debut;
         MaillonD temp2 = new MaillonD(element);
         if (element != null) {
-            while (!(element.equals(temp)) && temp != null) {
+            while (!(element.equals(temp.getInfo())) && temp != null) {
                 temp = temp.getSuivant();
             }
-            if (element.equals(temp)) {
+            if (element.equals(temp.getInfo())) {
+                while (temp.getDoublonSuivant() != null) {
+                    temp = temp.getDoublonSuivant();
+                }
                 temp.setDoublonSuivant(temp2);
             } else {
                 fin.setSuivant(temp2);
                 fin = temp2;
+                nbrElements++;
             }
         }
     }
 
     @Override
     public ArrayList<Object> obtenirDoublons(Object element) {
-        
+
         ArrayList<Object> listeRetour = new ArrayList<>();
-        MaillonD temp = new MaillonD(element);
-        temp = debut;
-        
-        if(element != null){
-            while(temp != null){
-                listeRetour.add(temp);
+        MaillonD temp = debut;
+
+        if (element != null) {
+            while (!element.equals(temp.getInfo()) && temp != null) {
                 temp = temp.getSuivant();
+            }
+            while (temp != null) {
+                listeRetour.add(temp);
+                temp = temp.getDoublonSuivant();
             }
         }
         return listeRetour;
@@ -64,42 +73,127 @@ public class ListeDoublonsChainee implements ListeDoublons<Object> {
 
     @Override
     public ArrayList<Object> obtenirElementsDistincts() {
-        
+
         ArrayList<Object> listeRetour = new ArrayList<>();
-        
-        
-        
+        MaillonD temp = debut;
+
+        while (temp != null) {
+            listeRetour.add(temp);
+            temp = temp.getSuivant();
+        }
         return listeRetour;
     }
 
     @Override
     public boolean supprimerTous(Object element) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        boolean retour = false;
+        MaillonD temp = debut;
+        MaillonD temp2;
+        if (element != null) {
+            while (!element.equals(temp.getSuivant().getInfo()) && temp != null) {
+                temp = temp.getSuivant();
+            }
+            if (temp != null) {
+                temp2 = temp;
+                temp = temp.getSuivant();
+                temp2.setSuivant(temp.getSuivant());
+                retour = true;
+            }
+            //regarde si le suivant du nouveau pointeur est null ou non
+            if (temp.getSuivant().getSuivant() == null) {
+                fin = temp.getSuivant();
+            }
+        }
+        setNbrEement();
+        return retour;
     }
 
     @Override
     public int nbrOccurrences(Object element) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int nbOccurence = 0;
+        MaillonD temp = debut;
+
+        if (element != null) {
+            while (!element.equals(temp.getInfo()) && temp != null) {
+                temp = temp.getSuivant();
+            }
+            while (temp != null) {
+                nbOccurence++;
+                temp = temp.getDoublonSuivant();
+            }
+        }
+        return nbOccurence;
     }
 
     @Override
     public boolean existe(Object element) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        boolean existe = false;
+        MaillonD temp = debut;
+
+        while (!element.equals(temp.getInfo()) && temp != null) {
+            temp = temp.getSuivant();
+            if (temp.getInfo().equals(element)) {
+                existe = true;
+            }
+        }
+        return existe;
     }
 
     @Override
     public void vider() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        debut = null;
+        fin = null;
+        nbrElements = 0;
     }
 
     @Override
     public Object premier() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return debut;
     }
 
     @Override
     public Object dernier() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        return fin;
     }
 
+    private void setNbrEement() {
+        MaillonD temp = debut;
+        int nbElement = 0;
+
+        while (temp != null) {
+            nbElement++;
+            temp = temp.getSuivant();
+        }
+        nbrElements = nbElement;
+    }
+
+    public String toString() {
+        String s = "";
+        MaillonD<T> p;
+        MaillonD<T> p2;
+
+        if (estVide()) {
+            s = "Liste vide";
+        } else {
+            p = debut;
+            while (p != null) {
+                s = s + p.getInfo();
+                if (p.getDoublonSuivant() != null|| p.getSuivant() != null) {
+                    s = s + ", ";
+                }
+                p2 = p;
+                //parcourir la liste des doublons, s'il y a lieu 
+                while (p2.getDoublonSuivant() != null) {
+                    p2 = p2.getDoublonSuivant();
+                    s = s + p2.getInfo();
+                    if (!(p == fin && p2.getDoublonSuivant() == null
+                            && p2.getSuivant() == null)) {
+                        s = s + ", ";
+                    }
+                }
+                p = p.getSuivant();
+            }
+        }
+        return s;
+    }
 }
